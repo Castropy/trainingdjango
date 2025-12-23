2b7eab57dc885f3fe9a21add8c6d0677d4b62f3c
Castropy <randrescastroi@gmail.com>
2025-12-23 00:25:47 -0400

    Modelo de la cancion

--- musica/models.py
+++ musica/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Cancion(models.Model):
+    nombre = models.CharField(max_length=200)
+    duracion = models.CharField(max_length=50)
+    artista = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+
