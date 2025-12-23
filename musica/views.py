fcccd90b2a769d869ec169109726ac125df4ade6
Castropy <randrescastroi@gmail.com>
2025-12-23 00:49:16 -0400

    serializer

--- musica/views.py
+++ musica/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Cancion
+from .serializers import CancionSerializer
+
+class CancionViewSet(viewsets.ModelViewSet):
+    queryset = Cancion.objects.all()
+    serializer_class = CancionSerializer
